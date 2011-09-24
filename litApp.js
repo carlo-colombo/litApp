1d4dbad370d811f69e7ea76f1903fb592453553b
Carlo Colombo <carlo.colombo@gmx.com>
2011-09-24 11:28:16 +0200

    extends $.couch.db with docInfo, some default options, debug logging

--- litApp.js
+++ litApp.js
@@ -2,9 +2,33 @@
     $.litApp = function(dbname,app,options){
         var that = this;
         that.options = {
-            session: true
+            session: true,
+            localStorage: 'localStorage' in window 
+                && window['localStorage'] !== null ,
+            debug: true
         };
+
+        //enabling ajax request loggin if debug=true
+        if(!!debug && window.console){
+            $.ajaxSetup({
+               success: function(message,status,response){
+                    console.log(message,status,response);
+               }
+            });
+        }
+        
         that.db = $.couch.db(dbname);
+        
+        //extends $.couch.db with docInfo method, HEAD method
+        $.extend(that.db,{
+            docInfo: function(id,options){
+                $.ajax($.extend(options,{
+                    url: this.uri + encodeURIComponent(id),
+                    type: 'head'
+                }));    
+            }
+        });
+        
         that.db.openDoc('_design/'+app,{success:function(design){
            $.extend(that.options,design,options);
            
@@ -23,5 +47,22 @@
                 $(window).trigger('designReady.litApp',that);
             }
         }});
+        
+        $.extend(that,{
+            openDoc:openDoc
+        })
+    }
+    
+    var openDoc =function(id){
+        var that=this;
+        if(window.navigator.onLine){
+            that.db.head(id);
+        }
+    }
+    
+    function trigger(event){
+        return function(data){
+            $(window).trigger(event,data);
+        }
     }
 })(jQuery);
