d7320bf929762c1c32a237d16708cfcd5928caaf
Carlo Colombo <carlo.colombo@gmx.com>
2011-11-01 13:00:10 +0100

    change on input creation, added number input type

--- Control.js
+++ Control.js
@@ -1,18 +1,32 @@
 ;(function($){
 	function makeInput(control){
-		return $('<input>',{
-				type: control.type  == 'edit' ? 'text': control.type,
-				value: control.default,
-				id: control.name,
-				name : control.name,
-				placeholder: control.description
-			});
+		var prop = $.extend(control,{
+			id: control.name,
+			placeholder: control.description
+		});
+
+		return $('<input>',prop);
 	}
 
 	$.litApp.Controls = {};
 
 	//export control type
-	$.each(['edit','text','time', 'password', 'email'],function(i,ctrl){
-		$.litApp.Controls[ctrl] = makeInput;		
+	$.each(['text','time','date','datetime', 'password', 'email','range','number'],function(i,ctrl){
+		$.litApp.Controls[ctrl] = field;
 	});
+
+	function field (control) {
+		var label = control.label || function(name){
+			return name.charAt(0).toUpperCase()
+				+ name.substring(1);
+		}
+
+		var $input = makeInput(control);
+		var $field = $('<div>').append($('<label>',{
+			for: control.name,
+			text: typeof label == "function" ? label(control.name) : label 
+		})).append($('<div>',{class : 'input' }).append($input));
+
+		return $field;
+	}
 })(jQuery);
