70611bfef5790184829e1592e029cba390b8fccd
Carlo Colombo <carlo.colombo@gmx.com>
2011-11-05 21:21:17 +0100

    main bar with dialog from template

--- _attachments/litApp/script.js
+++ _attachments/litApp/script.js
@@ -5,17 +5,30 @@
 				'http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js',
 				'http://twitter.github.com/bootstrap/1.3.0/bootstrap-tabs.js',
 				'http://twitter.github.com/bootstrap/1.3.0/bootstrap-modal.js',
-				'litApp.js',
-				'Control.js', 'Dialog.js','Bar.js',
+				'/litapp/_design/litApp/litApp/litApp.js',
+				'/litapp/_design/litApp/litApp/Control.js',
+				'/litapp/_design/litApp/litApp/Dialog.js',
+				'/litapp/_design/litApp/litApp/Bar.js',
 				'http://twitter.github.com/bootstrap/1.3.0/bootstrap.min.css'
 			],
 			complete: function(){
 				jQuery(function($){
-					$.litApp.Bar({
-						title: "Test",
-						dialogs : [d]
+
+					$.getJSON('/litapp/_design/litApp',function(design){
+						
+						var template = $('body').data('template');
+						var templateDef = design.config.templates[template];
+						var dialogs = design.config.dialog[templateDef.dialog || template];
+						if(typeof(dialogs) == "object" ){
+							dialogs = [dialogs]
+						}
+
+						$.litApp.Bar({
+							title: "Test",
+							dialogs : dialogs
+						})
+						.appendTo('body');	
 					})
-					.appendTo('body');
 				});
 			}
 		}
