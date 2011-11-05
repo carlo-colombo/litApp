8193609a6f0de40a3c6be4eba4e22496c8f8ca3a
Carlo Colombo <carlo.colombo@gmx.com>
2011-11-05 20:00:14 +0100

    cleanup

--- lists/test.js
+++
@@ -1,9 +0,0 @@
-function(head, req) {
-
-	
-  	var template = require('vendor/mustache.couch').compile(this, 'test');
-
-  	template.stream({title: 'Notes'}, function(row) {
-    	return row.doc;
-  	})  	
-}
--- shows/test.js
+++
@@ -1,4 +0,0 @@
-function(doc, req) {
-	var template = require('vendor/mustache.couch').compile(this, doc.metadata.template);
-	template.show(doc)
-}
