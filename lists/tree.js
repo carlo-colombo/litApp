22693519eef4a5c2804f6efa46441fe8feaf932e
Carlo Colombo <carlo.colombo@gmx.com>
2011-11-13 16:28:16 +0100

    admin tree view

--- lists/tree.js
+++ lists/tree.js
@@ -1,7 +1,28 @@
 function(head, req) {
-	var template = require('vendor/mustache.couch').compile(this,'admin_tree');
+	var template = require('vendor/mustache.couch').compile(this,'admin_tree',{
+		layout: 'admin_layout'
+	});
 
-	template.stream({title:'admin view'},function(row){
-		return row.value;
+	var ddoc = this;
+
+	template.stream({
+		title:'Admin',
+		subtitle:'Tree view'
+	},function(row){
+		var templates = [];
+
+		for(t in ddoc.config.templates){
+			templates[templates.length]={
+				name: ddoc.config.templates[t].name || t,
+				template: t,
+				selected: t == row.value.metadata.template
+			}
+		}
+
+		return {
+			level: row.value.metadata.path.length,
+			content: row.value,
+			templates: templates
+		}
 	});
 }
