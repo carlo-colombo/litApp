fcdd963337bec6f53051433ea628b17ecd39b747
Carlo Colombo <carlo.colombo@gmx.com>
2011-11-16 19:17:52 +0100

    some other metadata, refactor

--- updates/newPage.js
+++ updates/newPage.js
@@ -1,13 +1,17 @@
 function(doc,req){
-	doc = {},
-	doc._id = req.uuid,
-	doc.metadata = {
-		path : req.form['path'] 
-			? req.form['path'].split(',').concat(req.uuid)
-			: [req.uuid]
-	},
-	doc.metadata.template = Object.keys(this.config.templates)[0],
-	doc.name = 'untitled';
+
+	doc = {
+		"_id": req.uuid,
+		"name": "untitled",
+		"metadata": {
+			"path" : req.form['path'] 
+				? req.form['path'].split(',').concat(req.uuid)
+				: [req.uuid],
+			"template" : Object.keys(this.config.templates)[0],
+			"author": req.userCtx.name,
+			"created": Date()
+		}
+	}
 
 	return [doc,'lol'];
 }
