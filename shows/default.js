8c676934b2187f3ee8f6cdd219da496271c2e79b
Carlo Colombo <carlo.colombo@gmx.com>
2011-11-16 20:57:18 +0100

    litApp chenges fix

--- shows/default.js
+++ shows/default.js
@@ -1,7 +1,10 @@
 function(doc, req) {
 	var def = this.config.templates[doc.metadata.template],
 		mc = require('vendor/mustache.couch'),
-		litApp = new require('vendor/litApp').litApp(this)
+		litApp = new require('vendor/litApp').litApp({
+			design: this,
+			userCtx: req.userCtx
+		})
 	if(doc){
 		var template = mc.compile(this, def.template || doc.metadata.template);
 		template.show({
