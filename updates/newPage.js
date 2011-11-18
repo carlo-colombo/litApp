415205a491c7ada3c328bd9244f46eba736ad4e5
Carlo Colombo <carlo.colombo@gmx.com>
2011-11-18 21:06:37 +0100

    refactor

--- updates/newPage.js
+++ updates/newPage.js
@@ -1,12 +1,12 @@
 function(doc,req){
+	var p = req.form['path'],
+		path = p ? p.split(',') : [];
 
 	doc = {
 		"_id": req.uuid,
 		"name": "untitled",
 		"metadata": {
-			"path" : req.form['path'] 
-				? req.form['path'].split(',').concat(req.uuid)
-				: [req.uuid],
+			"path" : path.concat(req.uuid),
 			"template" : Object.keys(this.config.templates)[0],
 			"author": req.userCtx.name,
 			"created": Date(),
