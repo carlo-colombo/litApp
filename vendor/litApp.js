0fbf535d0b04f7150747a664518e9b0fc9c07f93
Carlo Colombo <carlo.colombo@gmx.com>
2011-11-23 23:28:42 +0100

    fix link to pages

--- vendor/litApp.js
+++ vendor/litApp.js
@@ -3,7 +3,7 @@ exports.litApp = function(opt){
 	this.design = opt.design,
 	this.userCtx = opt.userCtx,
 	this.link = function (doc){
-		return "/" + [this.userCtx.db,this.design._id,"_show/default",doc._id].join("/")
+		return "/" + [this.userCtx.db,this.design._id,"_show/default",doc.id].join("/")
 	},
 
 	this.base = '/'+ this.userCtx.db + '/' + this.design._id;
