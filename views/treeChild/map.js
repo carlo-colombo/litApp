b6209efaeb6bc58bff830e2a35a421c103ee7098
Carlo Colombo <carlo.colombo@gmx.com>
2011-11-23 23:11:38 +0100

    add _id to value

--- views/treeChild/map.js
+++ views/treeChild/map.js
@@ -3,7 +3,7 @@ function(doc) {
 	* emit parent node (root if empty) and data to build
 	* page tree
 	*/
-	emit([doc.metadata.path.slice(-2,-1)[0] || "root",doc.metadata.created], {
+	emit(doc._id, {
 		"id": doc._id,
 		"title": doc.title,
 		"name" : doc.name,
